008bb14901ce92493254db4eefe2b698d2b9462f
Jonathan Mendoza <john.2114@hotmail.com>
2024-06-24 16:31:11 +0200

    Add project files.

---
+++ IntroductionToLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IntroductionToLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers =  new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            oddNumers(numbers);
+            Console.ReadKey();
+            
+        }
+
+        static void oddNumers(int[] Numbers)
+        {
+            Console.WriteLine("Odd Numbers: " );
+            IEnumerable<int> oddNumbers = from number in Numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers);
+
+            foreach(int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
